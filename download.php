64b09402638d7bd818432f421e3793886dbb9eea
FarionT <farion2803@gmail.com>
2022-10-24 01:08:15 +0700

    responsive download.php

--- download.php
+++ download.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>G2X</title>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="styledown.css">
     <link 
         href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" 
         rel="stylesheet" 
@@ -14,36 +14,39 @@
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
 </head>
 <body>
-    <div class="container">
-        <?php require_once('header.html'); ?>
-        <div class="d-flex justify-content-around mt-5">
-            <button class="button-game align-self-center"><</button>
-            <div class="box-game"></div>
-            <button class="button-game align-self-center">></button>
-        </div>
-        <div class="d-flex flex-column align-items-center mt-5">
-            <h1 class="mx-auto mt-5">GAME TITLE GAME TITLE</h1>
-            <h3 class="mb-5">Team Name</h3>
-            <div>
-                <button class="button-choose rounded-pill py-2 me-3"><h4>DOWNLOAD</h4></button>
-                <button class="button-choose rounded-pill py-2"><h4>VOTE</h4></button>
+    <?php require_once('header.html'); ?>
+    <div class="container col col-lg-10">
+        <div class="container container-md-fluid">
+            <div class="d-flex justify-content-around mt-5">
+                <button class="btn btn-light border border-2 border-dark rounded-circle align-self-center"><</button>
+                <img class="border border-dark border-4 rounded-4 w-75" src="src/game_placeholder.png"></img>
+                <button class="btn btn-light border border-2 border-dark rounded-circle align-self-center">></button>
+            </div>
+            <div class="d-flex flex-column align-items-center mt-5">
+                <h1 class="mx-auto mt-5 text-center">GAME TITLE GAME TITLE</h1>
+                <h3 class="mb-5 text-center">Team Name</h3>
+                <div class="d-flex justify-content-center container-lg-fluid">
+                    <button class="button-choose rounded-pill py-2 mx-2"><p class="m-auto">DOWNLOAD</p></button>
+                    <button class="button-choose rounded-pill py-2 mx-2"><p class="m-auto">VOTE</p></button>
+                </div>
             </div>
         </div>
-        <div class="w-75 d-flex mx-auto mt-5">
-            <div class="d-flex flex-column">
-                <h3>ABOUT GAME</h3>
-                <p>Description Description Description Description Description Description Description Description Description Description</p>
+        
+        <div class=" d-flex mt-5 row row-2 row-lg-2 justify-content-lg-around justify-content-center">
+            <div class="col col-lg-6 col-12 mb-5">
+                <p class="h3">ABOUT GAME</p>
+                <p class="">Description Description Description Description Description Description Description Description Description Description</p>
             </div>
-            <div class="d-flex flex-column">
-                <h3>HOW TO PLAY</h3>
-                <p>Description Description Description Description Description Description Description Description Description Description</p>
+            <div class="col col-lg-6 col-12 ">
+                <p class="h3">HOW TO PLAY</p>
+                <p class="">Description Description Description Description Description Description Description Description Description Description</p>
             </div>
         </div>
-        <div class="w-75 d-flex flex-column mx-auto mt-5">
+        <div class="d-flex flex-column mx-auto mt-5">
             <h3 class="mb-5">CREATORS</h3>
-            <div class="d-flex">
-                <div style="margin-right: 220px">
-                    <div class="d-flex justify-content-between mb-3">
+            <div class="d-flex row row-2 ">
+                <div class="col col-lg-6 col-12">
+                    <div class="d-flex mb-3">
                         <div class="profile rounded-circle"></div>
                         <div class="ms-3">
                             <h5 class="mb-3">Name Name Name</h5>
@@ -51,7 +54,7 @@
                             <p>NIM NIM NIM(Jurusan)</p>
                         </div>
                     </div>
-                    <div class="d-flex justify-content-between mb-3">
+                    <div class="d-flex mb-3">
                         <div class="profile rounded-circle"></div>
                         <div class="ms-3">
                             <h5 class="mb-3">Name Name Name</h5>
@@ -59,7 +62,7 @@
                             <p>NIM NIM NIM(Jurusan)</p>
                         </div>
                     </div>
-                    <div class="d-flex justify-content-between mb-3">
+                    <div class="d-flex mb-3">
                         <div class="profile rounded-circle"></div>
                         <div class="ms-3">
                             <h5 class="mb-3">Name Name Name</h5>
@@ -68,8 +71,8 @@
                         </div>
                     </div>
                 </div>
-                <div>
-                    <div class="d-flex justify-content-between mb-3">
+                <div class="col col-lg-6 col-12">
+                    <div class="d-flex mb-3">
                         <div class="profile rounded-circle"></div>
                         <div class="ms-3">
                             <h5 class="mb-3">Name Name Name</h5>
@@ -77,7 +80,7 @@
                             <p>NIM NIM NIM(Jurusan)</p>
                         </div>
                     </div>
-                    <div class="d-flex justify-content-between mb-3">
+                    <div class="d-flex mb-3">
                         <div class="profile rounded-circle"></div>
                         <div class="ms-3">
                             <h5 class="mb-3">Name Name Name</h5>
@@ -85,7 +88,7 @@
                             <p>NIM NIM NIM(Jurusan)</p>
                         </div>
                     </div>
-                    <div class="d-flex justify-content-between mb-3">
+                    <div class="d-flex mb-3">
                         <div class="profile rounded-circle"></div>
                         <div class="ms-3">
                             <h5 class="mb-3">Name Name Name</h5>
