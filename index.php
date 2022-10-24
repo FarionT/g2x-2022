f3023093ba58b12aa232e01910259a5906209640
Axel Ferdinand <axel.ferdinand.88@gmail.com>
2022-10-24 16:40:58 +0700

    index responsive

--- index.php
+++ index.php
@@ -4,37 +4,15 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="indexstyles.css" rel="stylesheet" />
+        <link href="styles.css" rel="stylesheet" />
+        <link href="indexstyle3.css" rel="stylesheet" />
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://unpkg.com/flickity@2/dist/flickity.min.css">
         <title>G2X 2022</title> 
     </head>
     <body>
         <div class="container">
-            <nav class="d-flex justify-content-between w-75 mx-auto mt-5 navbar navbar-expand-lg navbar-light">
-                <div class="container-fluid">
-                    <button id="header-button"></button>
-                    <button class="navbar-toggler" type="button"
-                     data-bs-toggle="collapse"
-                     data-bs-target="#navbarSupportedContent"
-                     aria-controls="navabarSupportedContent"
-                     aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                            <li class="nav-item">
-                                <a class="nav-link active  header-text me-md-5" aria-current="page" href="#">Games</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link active  header-text me-md-5" aria-current="page" href="#">Competition</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link active  header-text" aria-current="page" href="#">Webinar</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
+           <?php require_once('header.html'); ?>
             <div id="judul" class="mt-5">
                 <div class="d-flex flex-md-column flex-row mb-md-0 mb-3">
                     <h1 class="mb-0" id="judul-welcome">WELCOME TO&nbsp;</h1>
@@ -83,21 +61,25 @@
             <br /><br /><br />
             <h2 class="button-arrow text-center game-entries-def mb-5">GAME ENTRIES →</h2>
             <h2 class="button-arrow text-center game-entries-resp mb-5">GAME ENTRIES</h2>
-            <div class="d-flex justify-content-md-around game-entries-div">
-                <button class="button-game-entries align-self-center">&lt</button>
-                <div>
-                    <div id="game-entries-1" class="d-flex justify-content-md-around mb-5"> 
-                        <div class="game-entries-box mb-5 mb-md-0 me-md-5">1</div>
-                        <div class="game-entries-box mb-5 mb-md-0">2</div>
-                        <div class="game-entries-box ms-md-5">3</div>
+            <div class="body-slider">
+                <div id="slider" class="slider d-flex justify-content-md-around game-entries-div">
+                    <button class="button-game-entries align-self-center">&lt</button>
+                    <div class="wrapper">
+                        <div id="slides" class="slides game-entries-1 d-flex justify-content-md-around mb-5"> 
+                            <div class="slide game-entries-box mb-5 mb-md-0 me-md-5">Slide 1</div>
+                            <div class="slide game-entries-box mb-5 mb-md-0">Slide 2</div>
+                            <div class="slide game-entries-box ms-md-5">Slide 3</div>
+                        </div>
+                        <div id="game-entries-2">
+                            <div class="game-entries-box me-5"></div>
+                            <div class="game-entries-box"></div>
+                            <div class="game-entries-box ms-5"></div>
+                        </div>
                     </div>
-                    <div id="game-entries-2">
-                        <div class="game-entries-box me-5"></div>
-                        <div class="game-entries-box"></div>
-                        <div class="game-entries-box ms-5"></div>
-                    </div>
+                    <a id="prev" class="control prev"></a>
+                    <a id="next" class="control next"></a>
+                    <button class="button-game-entries align-self-center">&gt</button>
                 </div>
-                <button class="button-game-entries align-self-center">&gt</button>
             </div>
             <br /><br /><br /><br /><br /><br /><br /><br /><br />
             <h2 class="button-arrow text-center mt-5 mb-3">MEDIA PARTNERS</h2>
@@ -126,8 +108,9 @@
                 <p>&copy 2022 G2X. All rights reserved.</p>
             </div>
         </div>
+        <script src="https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js"></script>
         <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-        <script src="javascript.js"></script>
+        <script src="javaindex1.js"></script>
     </body>
 </html>
