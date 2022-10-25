1064f9179d8f2b8b8ebf5f467ef635019fdcbb3a
Axel Ferdinand <axel.ferdinand.88@gmail.com>
2022-10-26 01:16:49 +0700

    insert asset to index

--- index.php
+++ index.php
@@ -5,58 +5,60 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="styles.css" rel="stylesheet" />
-        <link href="indexstyle5.css" rel="stylesheet" />
+        <link href="indexstyle7.css" rel="stylesheet" />
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
         <link rel="stylesheet" href="https://unpkg.com/flickity@2/dist/flickity.min.css">
         <title>G2X 2022</title> 
         <style>
-            body{background-image: url("src/bg-website-landing-page.png");}
+            body{background-image: url("src/bg-website-landing-page.png"); background-size: 1600px;}
         </style>
     </head>
     <body>
         <div class="container">
-           <?php require_once('header.html'); ?>
+            <header>
+                <?php require_once('header.html'); ?>
+            </header>
             <div id="judul" class="mt-5">
                 <div class="d-flex flex-xl-column flex-row mb-xl-0 mb-3">
                     <h1 class="mb-0" id="judul-welcome">WELCOME TO&nbsp;</h1>
                     <h1 class="mt-0" id="judul-g2x">G2X</h1> <br />
                 </div>
                 <p class="mb-0">Copywriting Copywriting Copywriting</p>
-                <p class="mt-0 mb-5">Copywriting Copywriting Copywriting</p>
-                <button href="" id="judul-button">START</button>
+                <p class="mt-0" style="margin-bottom: 100px;">Copywriting Copywriting Copywriting</p>
+                <a href="#judulsubjudul" id="judul-button"><img class="button-logo" src="src/index/awan_start.png" />START</a>
             </div>
-            <br /><br /><br /><br /><br /><br /><br /><br /><br />
-            <br /><br /><br /><br /><br /><br /><br /><br /><br />
-            <br /><br /><br />
             <div class="d-flex flex-column align-items-center mb-5">
-                <h2>G2X</h2>
+                <h2 id="judulsubjudul">G2X</h2>
                 <p class="mb-0">Copywriting Copywriting Copywriting</p>
                 <p class="mt-0 mb-5">Copywriting Copywriting Copywriting</p>
             </div>
             <div class="d-flex flex-column flex-lg-row justify-content-lg-around">
                 <div id="games">
-                    <button class="button-arrow button-arrow-resp mb-5">→</button>
+                    <img src="src/index/gameboy.png" class="logo"/>
+                    <button class="button-arrow button-arrow-resp mb-5"><img class="img-button-arrow" src="src/index/awan_arrow.png" /></button>
                     <h2 class="sub-judul">GAMES</h2>
                     <p class="mb-0">Copywriting Copywriting Copywriting</p>
                     <p class="mb-0">Copywriting Copywriting Copywriting</p>
                     <p class="mt-0 mb-5">Copywriting Copywriting Copywriting</p>
-                    <button class="button-arrow button-arrow-def">→</button>
+                    <button class="button-arrow button-arrow-def"><img class="img-button-arrow" src="src/index/awan_arrow.png" /></button>
                 </div>
                 <div id="competitions">
-                    <button class="button-arrow button-arrow-resp mb-5">→</button>
+                    <img src="src/index/piala.png" class="logo"/>
+                    <button class="button-arrow button-arrow-resp mb-5"><img class="img-button-arrow" src="src/index/awan_arrow.png" /></button>
                     <h2 class="sub-judul">COMPETITIONS</h2>
                     <p class="mb-0">Copywriting Copywriting Copywriting</p>
                     <p class="mb-0">Copywriting Copywriting Copywriting</p>
                     <p class="mt-0 mb-5">Copywriting Copywriting Copywriting</p>
-                    <button class="button-arrow button-arrow-def">→</button>
+                    <button class="button-arrow button-arrow-def"><img class="img-button-arrow" src="src/index/awan_arrow.png" /></button>
                 </div>
                 <div id="webinar">
-                    <button class="button-arrow button-arrow-resp mb-5">→</button>
+                    <img src="src/index/laptop.png" class="logo"/>
+                    <button class="button-arrow button-arrow-resp mb-5"><img class="img-button-arrow" src="src/index/awan_arrow.png" /></button>
                     <h2 class="sub-judul">WEBINAR</h2>
                     <p class="mb-0">Copywriting Copywriting Copywriting</p>
                     <p class="mb-0">Copywriting Copywriting Copywriting</p>
                     <p class="mt-0 mb-5">Copywriting Copywriting Copywriting</p>
-                    <button class="button-arrow button-arrow-def">→</button>
+                    <button class="button-arrow button-arrow-def"><img class="img-button-arrow" src="src/index/awan_arrow.png" /></button>
                 </div>
             </div>
             <br /><br /><br /><br /><br /><br /><br /><br /><br />
@@ -99,15 +101,7 @@
             </div>
             <br /><br /><br /><br /><br /><br /><br /><br /><br />
             <br /><br /><br /><br /><br /><br /><br /><br /><br />
-            <div class="d-flex flex-column align-items-center">
-                <h2>CONTACT US</h2>
-                <div class="d-flex">
-                    <button id="button-contact"></button>
-                    <p class="ms-3 mb-0">GDCUMN</p>
-                </div>
-                <br />
-                <p>&copy 2022 G2X. All rights reserved.</p>
-            </div>
+            <?php require_once('footer.html'); ?>
         </div>
         <script src="https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js"></script>
         <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
